f621a5cb7e75c27cfdaf25e900ae72b2352343e6
Diego Perez <diego.perez.nadler@gmail.com>
2019-08-05 18:19:50 -0300

    Done v1

--- handler.js
+++ handler.js
@@ -1,101 +1,102 @@
 'use strict';
 
-const uuid = require('uuid');
-
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.logList = async event => {
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+    };
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-  };
+    const data = await dynamoDb.scan(params).promise();
 
-  return dynamoDb.scan(params).promise()
-    .then(data => {
-      console.log(data);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data.Items),
-      };
-    }).catch(err => {
-      console.log(err);
-      return {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todos.',
-      };
-  });
+    console.log(data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items)
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      statusCode: error.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t fetch the todos. Message: ' + err.message
+    };
+  }
 };
 
 module.exports.logDetails = async event => {
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Key: {
-      id: event.pathParameters.id,
-    },
-  };
+  try {
 
-  return dynamoDb.get(params).promise()
-    .then(data => {
-      console.log(data);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data.Item),
-      };
-    }).catch(err => {
-      console.log(err);
-      return {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todos.',
-      };
-  });
+    console.log(event);
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        id: event.pathParameters.id,
+      },
+    };
+    
+    const data = await dynamoDb.get(params).promise();
+    
+    console.log(data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Item)
+    }
+  
+  } 
+  catch (err) {
+    
+    console.log(err);
+    
+    return {
+      statusCode: error.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t fetch the todos.',
+    };
+  }
+
 };
 
 module.exports.logCreate = async event => {
-  // creation Time
-  const timestamp = new Date().getTime();
 
-  // Parsing data
-  const data = JSON.parse(event.body);
-  
-  // if (typeof data.text !== 'string') {
-  //   console.error('Validation Failed');
-  //   return {
-  //     statusCode: 400,
-  //     headers: { 'Content-Type': 'text/plain' },
-  //     body: 'Couldn\'t create the todo item.',
-  //   };
-  // }
-
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: data
-  };
-
-  // write the todo to the database
-  return dynamoDb.put(params).promise()
-    .then(data => {
+  try {
+    // Parsing data
+    const data = JSON.parse(event.body);
     
-      console.log('Item created: ' + params.Item);
-
-      // create a response
-      return {
-        statusCode: 200,
-        body: JSON.stringify(params.Item),
-      };
-    }).catch(err => {
-      console.error(err);
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: data
+    };
+    
+    const result = await dynamoDb.put(params).promise();
+    
+    console.log('Item created: ', params.Item, ' Data response: ', result);
+    
+    
+    // create a response
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    };
+  }
+  catch (err) {
+    console.log(err);
       
-      return {
-        statusCode: err.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the todo item.',
-      };
-    });
+    return {
+      statusCode: error.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t fetch the todos.',
+    };
+  }
+
 };
 
 module.exports.logDelete = async event => {
